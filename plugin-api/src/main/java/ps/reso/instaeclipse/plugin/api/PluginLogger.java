bfe929d4c4ebdb550f1027478ef827f4e3ba2b26
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-09-03 01:38:21 +0530

    chore: remove obsolete plugin api module

--- plugin-api/src/main/java/ps/reso/instaeclipse/plugin/api/PluginLogger.java
+++
@@ -1,7 +0,0 @@
-package ps.reso.instaeclipse.plugin.api;
-
-public interface PluginLogger {
-    void info(String message);
-    void warn(String message);
-    void error(String message, Throwable throwable);
-}
