5c741daba003b2b01e84bf4c096b0f6b19b609ef
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-09-03 01:38:12 +0530

    chore: remove obsolete plugin api module

--- plugin-api/src/main/java/ps/reso/instaeclipse/plugin/api/InstaEclipsePlugin.java
+++
@@ -1,9 +0,0 @@
-package ps.reso.instaeclipse.plugin.api;
-
-/** Stable executable-plugin contract. Keep this API backwards compatible. */
-public interface InstaEclipsePlugin {
-    String getId();
-    String getVersion();
-    void onLoad(PluginContext context) throws Throwable;
-    default void onUnload() throws Throwable {}
-}
